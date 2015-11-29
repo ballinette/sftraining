ca82cfe85c38d3ca1bda92cf0d06df9256bce7c4
ballinette <gballini@kaliop.com>
2015-11-29 04:38:58 +0100

    Create your first TrainingBundle
    php app/console generate:bundle

---
+++ src/My/TrainingBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace My\TrainingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('MyTrainingBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
